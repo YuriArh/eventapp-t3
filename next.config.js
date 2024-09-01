705917e95c7a4721f7f2ee385156eacd9b2b7be1
yuarkhipov <yuarkhipov@team.amocrm.com>
2024-09-01 21:24:32 +0300

    [fix] argon deploy 2

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingIncludes: {
+    "/": ["./node_modules/argon2/prebuilds/linux-x64/*.musl.*"],
+  },
 };
 
 export default withNextIntl(nextConfig);
